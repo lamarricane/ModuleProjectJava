abc9b176077009262a54b245e026759103ea2745
lamarricane <lamarricane@gmail.com>
2025-05-13 00:43:00 +0400

    вроде работает

---
+++ api-gateway/src/main/java/com/example/config/MonitoringConfig.java
@@ -0,0 +1,16 @@
+package com.example.config;
+
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MonitoringConfig {
+
+    @Bean
+    public MeterRegistryCustomizer<PrometheusMeterRegistry> customizeService1() {
+        return registry -> registry.config()
+                .commonTags("application", "api-gateway");
+    }
+}
---
+++ catalog-service/src/main/java/com/example/config/MonitoringConfig.java
@@ -0,0 +1,16 @@
+package com.example.config;
+
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MonitoringConfig {
+
+    @Bean
+    public MeterRegistryCustomizer<PrometheusMeterRegistry> customizeService1() {
+        return registry -> registry.config()
+                .commonTags("application", "catalog-service");
+    }
+}
